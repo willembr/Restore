c4efcfb13b0b94d99535a8f6dc0c3e03347dd87a
Willem Brants <willembrants@hotmail.com>
2024-08-02 10:11:46 +0200

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,48 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<StoreContext>(opt => 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+
+//WE CREATE NEXT BECAUSE WE NEED TO GET HOLD OF OUR DB CONTEXT SERVICE, BUT WE CANNOT INJECT OUR STORE CONTEXT INSIDE
+//THIS CLASS
+var scope = app.Services.CreateScope();
+
+var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+try
+{
+    context.Database.Migrate();
+    DbInitializer.Initialize(context);
+}
+catch (Exception ex)
+{
+    
+   logger.LogError(ex, "A problem occured during migration");
+}
+
+app.Run();
